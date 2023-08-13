ef9417607ec802f75ba787e1edd4064a3cd2dc37
Joseph Simpson <joe@radicalappdev.com>
2023-08-13 06:36:04 -0400

    Basic routs for benches
    I'm not a fan of this new method of creating routes in Next JS

---
+++ app/benches/1000/page.js
@@ -0,0 +1,7 @@
+export default function Bench() {
+  return (
+    <main>
+      <p>Hello bences</p>
+    </main>
+  );
+}
---
+++ app/benches/1001/page.js
@@ -0,0 +1,7 @@
+export default function Bench() {
+  return (
+    <main>
+      <p>Hello bence 1001</p>
+    </main>
+  );
+}
