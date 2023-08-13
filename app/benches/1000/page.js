8673230d1248e341e16cd5d91eebef8849d83dba
Joseph Simpson <joe@radicalappdev.com>
2023-08-13 07:11:29 -0400

    Basic bench and basic lab

--- app/benches/1000/page.js
+++ app/benches/1000/page.js
@@ -1,7 +1,20 @@
+"use client";
+
+import * as THREE from "three";
+import { OrbitControls, Box } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 export default function Bench() {
   return (
-    <main>
-      <p>Hello bences</p>
-    </main>
+    <>
+      <Canvas camera={{ position: [0, 3, 5] }}>
+        <color attach="background" args={["#eeeeee"]} />
+        <OrbitControls />
+        <primitive object={new THREE.GridHelper(10, 10)} />
+        <Box>
+          <meshBasicMaterial color="purple" />
+        </Box>
+      </Canvas>
+    </>
   );
 }
---
+++ app/labs/1000/page.js
@@ -0,0 +1,28 @@
+"use client";
+
+import * as THREE from "three";
+import { OrbitControls, Box } from "@react-three/drei";
+import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
+import { Canvas } from "@react-three/fiber";
+
+export default function Bench() {
+  return (
+    <>
+      <VRButton />
+      <Canvas camera={{ position: [0, 3, 5] }}>
+        <XR>
+          <color attach="background" args={["#eeeeee"]} />
+          <Controllers />
+          <Hands />
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <directionalLight intensity={1} />
+          <primitive object={new THREE.GridHelper(10, 10)} />
+          <Box>
+            <meshStandardMaterial color="purple" />
+          </Box>
+        </XR>
+      </Canvas>
+    </>
+  );
+}
