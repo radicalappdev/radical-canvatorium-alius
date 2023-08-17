3611a37d645f59be8f531e7023f421760629481b
Joseph Simpson <joe@radicalappdev.com>
2023-08-17 17:07:34 -0400

    Adding some text to Lab 1000

--- src/pages/labs/1000.jsx
+++ src/pages/labs/1000.jsx
@@ -2,7 +2,7 @@
 
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Box } from "@react-three/drei";
+import { OrbitControls, Text } from "@react-three/drei";
 import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
 import { labColors } from "@/utils/labTheme";
 
@@ -10,6 +10,7 @@ export default function Lab1000() {
   return (
     <>
       <VRButton />
+      {/* Camera position is set in flat mode, but when I enter XR the player appears at 0, 0, 0 */}
       <Canvas camera={{ position: [0, 3, 5] }}>
         <XR>
           <color attach="background" args={[labColors.slate1]} />
@@ -19,9 +20,19 @@ export default function Lab1000() {
           <ambientLight intensity={0.5} />
           <directionalLight intensity={1} />
           <primitive object={new THREE.GridHelper(20, 20)} />
-          <Box>
-            <meshStandardMaterial color="purple" />
-          </Box>
+
+          {/* create a plane with a transparent material */}
+
+          <mesh position={[0, 1.4, 0]}>
+            <planeGeometry attach="geometry" args={[4, 2]} />
+            <meshBasicMaterial attach="material" color={labColors.slate1} transparent opacity={0.8} />
+            <Text font="/fonts/NotoSans-Bold.ttf" color={labColors.slate8} anchorX="center" anchorY="middle" scale={[0.3, 0.3, 0.3]} position={[0, 0.2, 0.01]}>
+              Canvatorium Alius
+            </Text>
+            <Text font="/fonts/NotoSans-Bold.ttf" color={labColors.slate8} anchorX="center" anchorY="middle" scale={[0.15, 0.15, 0.15]} position={[0, -0.2, 0.01]}>
+              React + Three.js Edition
+            </Text>
+          </mesh>
         </XR>
       </Canvas>
     </>
