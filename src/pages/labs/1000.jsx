1d99facb3c7948e9fa9db8378e1de4282bf2b633
Joseph Simpson <joe@radicalappdev.com>
2023-08-17 15:58:42 -0400

    Adding lab theme

--- src/pages/labs/1000.jsx
+++ src/pages/labs/1000.jsx
@@ -4,6 +4,7 @@ import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Box } from "@react-three/drei";
 import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
+import { labColors } from "@/utils/labTheme";
 
 export default function Lab1000() {
   return (
@@ -11,13 +12,13 @@ export default function Lab1000() {
       <VRButton />
       <Canvas camera={{ position: [0, 3, 5] }}>
         <XR>
-          <color attach="background" args={["#eeeeee"]} />
+          <color attach="background" args={[labColors.slate1]} />
           <Controllers />
           <Hands />
           <OrbitControls />
           <ambientLight intensity={0.5} />
           <directionalLight intensity={1} />
-          <primitive object={new THREE.GridHelper(10, 10)} />
+          <primitive object={new THREE.GridHelper(20, 20)} />
           <Box>
             <meshStandardMaterial color="purple" />
           </Box>
