13a8f830ac9930cb83f6b5be6d09773bef113b15
Joseph Simpson <joe@radicalappdev.com>
2023-08-17 15:33:56 -0400

    Adding lab 1000

---
+++ src/pages/benches/index.jsx
@@ -0,0 +1,8 @@
+// A basic page that will print a list of all rountes in the bench folder
+// This is a NEXTJS app using the pages folder to create routes
+
+import React from "react";
+
+export default function Benches() {
+  return <div>all a list of pages here</div>;
+}
