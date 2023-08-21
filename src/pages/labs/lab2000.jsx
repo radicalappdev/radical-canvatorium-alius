38582abc9b94d64596712d39c13fc8c92edf8df0
Joseph Simpson <joe@radicalappdev.com>
2023-08-21 17:40:06 -0400

    Renaming some benches. Working on LabCamera

--- src/pages/labs/lab2000.jsx
+++ src/pages/labs/lab2000.jsx
@@ -1,29 +1,28 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Text } from "@react-three/drei";
 import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
+import { Text } from "@react-three/drei";
 import { labColors } from "@/utils/labTheme";
+import { LabCamera } from "@/components/LabCamera";
 
 export default function Lab1000() {
   return (
     <>
       <VRButton />
-      {/* Camera position is set in flat mode, but when I enter XR the player appears at 0, 0, 0 */}
-      <Canvas camera={{ position: [0, 3, 5] }}>
+      <Canvas>
         <XR>
+          <LabCamera />
           <color attach="background" args={[labColors.slate1]} />
           <Controllers />
           <Hands />
-          <OrbitControls />
+
           <ambientLight intensity={0.5} />
           <directionalLight intensity={1} />
 
-          {/* <gridHelper args={[20, 20, labColors.slate8, labColors.slate4]} /> */}
+          <gridHelper args={[20, 20, labColors.slate8, labColors.slate4]} />
 
-          <polarGridHelper args={[20, 20, 20, 20, labColors.slate8, labColors.slate4]} />
-
-          <mesh position={[0, 1.4, 0]}>
+          <mesh position={[0, 1.4, 0]} scale={[0.5, 0.5, 0.5]}>
             <planeGeometry attach="geometry" args={[4, 2]} />
             <meshBasicMaterial attach="material" color={labColors.slate1} transparent opacity={0.8} />
             <Text font="/fonts/NotoSans-Bold.ttf" color={labColors.slate8} anchorX="center" anchorY="middle" scale={[0.3, 0.3, 0.3]} position={[0, 0.2, 0.01]}>
