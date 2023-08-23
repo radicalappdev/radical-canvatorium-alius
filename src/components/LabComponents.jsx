ee5c84216a6ee4aedd6cd1f553e73a001f8e790e
Joseph Simpson <joe@radicalappdev.com>
2023-08-23 08:16:58 -0400

    Document another failure

--- src/components/LabComponents.jsx
+++ src/components/LabComponents.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { labColors } from "@/utils/labTheme";
-import { Hands, Controllers, useXR, useTeleportation } from "@react-three/xr";
+import { Hands, Controllers, TeleportationPlane, useXR, useTeleportation } from "@react-three/xr";
 
 export function LabCamera(props) {
   const { position } = props;
@@ -29,16 +29,20 @@ export function LabRoom() {
 
 export function LabXRPlayer(props) {
   const { position } = props;
-  const player = useXR((state) => state.player);
-  player.position.set(...position);
 
-  const teleport = useTeleportation({ leftHanded: true });
+  // THIS IS THE ONLY THING THAT HAS LET ME POSITION THE PLAYER
+  // const player = useXR((state) => state.player);
+  // player.position.set(...position);
+
+  // THIS DOESN'T SEEM TO DO ANYTHING
+  const teleport = useTeleportation();
   teleport(...position);
 
   return (
     <>
       <Hands />
       <Controllers />
+      <TeleportationPlane leftHand rightHand />
     </>
   );
 }
