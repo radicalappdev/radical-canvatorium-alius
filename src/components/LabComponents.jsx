6f0e5ee937a2397e0b0c0973f3126aaa8c3c7d05
Joseph Simpson <joe@radicalappdev.com>
2023-08-23 05:32:46 -0400

    Trying useTeleportation
    Still failing

--- src/components/LabComponents.jsx
+++ src/components/LabComponents.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { labColors } from "@/utils/labTheme";
-import { Hands, Controllers, TeleportationPlane, useXR } from "@react-three/xr";
+import { Hands, Controllers, useXR, useTeleportation } from "@react-three/xr";
 
 export function LabCamera(props) {
   const { position } = props;
@@ -32,11 +32,13 @@ export function LabXRPlayer(props) {
   const player = useXR((state) => state.player);
   player.position.set(...position);
 
+  const teleport = useTeleportation({ leftHanded: true });
+  teleport(...position);
+
   return (
     <>
       <Hands />
       <Controllers />
-      <TeleportationPlane leftHand rightHand />
     </>
   );
 }
